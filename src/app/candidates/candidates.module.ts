8078a825394092f1fca1c202df0ec50a27ee0986
Andrés Isaza <andres.isaza1506@gmail.com>
2019-12-24 15:31:17 -0500

    created candidates form component, imported routing and reactive forms modules

--- src/app/candidates/candidates.module.ts
+++ src/app/candidates/candidates.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CandidatesRoutingModule } from './candidates.routes';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { CandidateComponent } from './components/candidate/candidate.component';
 import { CandidatesComponent } from './components/candidates/candidates.component';
+import { FormComponent } from './components/form/form.component';
 
 
 
 @NgModule({
   declarations: [
     CandidateComponent,
-    CandidatesComponent
+    CandidatesComponent,
+    FormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CandidatesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CandidatesModule { }
