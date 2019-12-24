496db3dc8be55e15c6ed34e0c828ab264a7d99e8
Andrés Isaza <andres.isaza1506@gmail.com>
2019-12-23 23:55:37 -0500

    project cleaned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pi1-frontend';
-}
+export class AppComponent { }
